d55f089581c8de54be1bc9cb7fd62002eaf7d6fa
Colin Huber <cbhuber@gmail.com>
2023-06-19 15:54:48 -0600

    feat: props and styling.

--- src/index.js
+++ src/index.js
@@ -69,8 +69,26 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese."
+        photoName="pizzas/spinaci.jpg"
+        price={10} // JS mode to enter numbers
+      />
     </main>
+  );
+}
+
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
+    </div>
   );
 }
 
@@ -84,16 +102,6 @@ function Footer() {
     <footer className="footer">
       {new Date().toLocaleTimeString("en-US")} We're currently open
     </footer>
-  );
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-      <h3>Pizza Pinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese.</p>
-    </div>
   );
 }
 
